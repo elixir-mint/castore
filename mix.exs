49ba3e0d2e28e3f722fa8fa030fde2f67a9570ee
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-11-15 19:55:25 +0100

    Add certificate generation

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule CAStore.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8-dev",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -24,5 +25,32 @@ defmodule CAStore.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
+  end
+
+  defp aliases do
+    [certdata: &certdata/1]
+  end
+
+  @mk_ca_bundle_url "https://raw.githubusercontent.com/curl/curl/master/lib/mk-ca-bundle.pl"
+  @mk_ca_bundle_cmd "mk-ca-bundle.pl"
+  @ca_bundle "ca-bundle.crt"
+  @ca_bundle_target "priv/cacerts.pem"
+
+  defp certdata(_) do
+    cmd("wget", [@mk_ca_bundle_url])
+    File.chmod!(@mk_ca_bundle_cmd, 0o755)
+
+    cmd(Path.expand(@mk_ca_bundle_cmd), ["-u"])
+
+    File.rename(@ca_bundle, @ca_bundle_target)
+    File.rm!(@mk_ca_bundle_cmd)
+  end
+
+  defp cmd(cmd, args) do
+    {_, result} = System.cmd(cmd, args, into: IO.stream(:stdio, :line), stderr_to_stdout: true)
+
+    if result != 0 do
+      raise "Non-zero result (#{result}) from: #{cmd} #{Enum.map_join(args, " ", &inspect/1)}"
+    end
   end
 end
