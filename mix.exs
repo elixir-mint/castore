bc29974e417c42e3486f92404f747a05b44fb00c
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2018-11-15 19:59:47 +0100

    Support Elixir 1.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CAStore.MixProject do
     [
       app: :castore,
       version: "0.1.0",
-      elixir: "~> 1.8-dev",
+      elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases()
