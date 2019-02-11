e53f4a2d43dea68bbbceb8649e8d9d6e3aa46937
Andrea Leopardi <an.leopardi@gmail.com>
2019-02-11 18:50:13 +0100

    Move the :files key in the correct place in mix.exs

--- mix.exs
+++ mix.exs
@@ -13,7 +13,6 @@ defmodule CAStore.MixProject do
       deps: deps(),
 
       # Hex
-      files: ["lib/castore.ex", "priv", "mix.exs", "README.md"],
       package: package(),
       description: "Up-to-date CA certificate store.",
 
@@ -41,6 +40,7 @@ defmodule CAStore.MixProject do
 
   defp package do
     [
+      files: ["lib/castore.ex", "priv", "mix.exs", "README.md"],
       licenses: ["Apache 2.0"],
       links: %{"GitHub" => @repo_url}
     ]
