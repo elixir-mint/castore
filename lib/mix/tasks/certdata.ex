c358367ffb46d612df6a26c063f0990733c0d9c6
Andrea Leopardi <an.leopardi@gmail.com>
2019-02-11 18:01:21 +0100

    Introduce "mix certdata --check-outdated" (#7)

---
+++ lib/mix/tasks/certdata.ex
@@ -0,0 +1,82 @@
+defmodule Mix.Tasks.Certdata do
+  @moduledoc """
+  Fetches an up-to-date version of the CA certificate store.
+
+  The certificate store is then stored in the private storage of this library. The path
+  to the CA certificate store can be retrieved through `CAStore.file_path/0`.
+
+  ## Usage
+
+  You can use this task to fetch an up-to-date version of the CA certificate store.
+
+      mix certdata
+
+  You can also use this task to verify if the currently stored certificate store is
+  up to date. To do that, use the `--check-outdated` option.
+
+      mix certdata --check-outdated
+
+  """
+
+  use Mix.Task
+
+  @shortdoc "Fetches an up-to-date version of the CA certificate store"
+
+  @mk_ca_bundle_url "https://raw.githubusercontent.com/curl/curl/master/lib/mk-ca-bundle.pl"
+  @mk_ca_bundle_cmd "mk-ca-bundle.pl"
+  @ca_bundle "ca-bundle.crt"
+  @ca_bundle_target "priv/cacerts.pem"
+
+  @impl true
+  def run(args)
+
+  def run([]) do
+    fetch_ca_bundle()
+    File.rename(@ca_bundle, @ca_bundle_target)
+  end
+
+  def run(["--check-outdated"]) do
+    fetch_ca_bundle()
+
+    old_bundle = read_certificates_set(CAStore.file_path())
+    new_bundle = read_certificates_set(@ca_bundle)
+
+    File.rm!(@ca_bundle)
+
+    if not MapSet.equal?(old_bundle, new_bundle) do
+      Mix.raise("#{CAStore.file_path()} is outdated. Run \"mix certfile\" to update it.")
+    end
+  end
+
+  def run(_args) do
+    Mix.raise("Invalid arguments. See `mix help certdata`.")
+  end
+
+  defp fetch_ca_bundle do
+    cmd!("wget #{@mk_ca_bundle_url}")
+
+    try do
+      File.chmod!(@mk_ca_bundle_cmd, 0o755)
+      cmd!("#{Path.expand(@mk_ca_bundle_cmd)} -u")
+    after
+      File.rm!(@mk_ca_bundle_cmd)
+    end
+  end
+
+  defp cmd!(cmd) do
+    Mix.shell().info([:magenta, "Running: #{cmd}"])
+
+    exit_status = Mix.shell().cmd(cmd)
+
+    if exit_status != 0 do
+      Mix.raise("Non-zero result (#{exit_status}) from command: #{cmd}")
+    end
+  end
+
+  defp read_certificates_set(path) do
+    path
+    |> File.read!()
+    |> :public_key.pem_decode()
+    |> MapSet.new()
+  end
+end
--- mix.exs
+++ mix.exs
@@ -11,9 +11,9 @@ defmodule CAStore.MixProject do
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases(),
 
       # Hex
+      files: ["lib/castore.ex", "priv", "mix.exs", "README.md"],
       package: package(),
       description: "Up-to-date CA certificate store.",
 
@@ -39,39 +39,10 @@ defmodule CAStore.MixProject do
     ]
   end
 
-  defp aliases do
-    [certdata: &certdata/1]
-  end
-
   defp package do
     [
       licenses: ["Apache 2.0"],
       links: %{"GitHub" => @repo_url}
     ]
-  end
-
-  @mk_ca_bundle_url "https://raw.githubusercontent.com/curl/curl/master/lib/mk-ca-bundle.pl"
-  @mk_ca_bundle_cmd "mk-ca-bundle.pl"
-  @ca_bundle "ca-bundle.crt"
-  @ca_bundle_target "priv/cacerts.pem"
-
-  defp certdata(_args) do
-    cmd!("wget #{@mk_ca_bundle_url}")
-    File.chmod!(@mk_ca_bundle_cmd, 0o755)
-
-    cmd!("#{Path.expand(@mk_ca_bundle_cmd)} -u")
-
-    File.rename(@ca_bundle, @ca_bundle_target)
-    File.rm!(@mk_ca_bundle_cmd)
-  end
-
-  defp cmd!(cmd) do
-    Mix.shell().info([:magenta, "Running: #{cmd}"])
-
-    exit_status = Mix.shell().cmd(cmd)
-
-    if exit_status != 0 do
-      Mix.raise("Non-zero result (#{exit_status}) from command: #{cmd}")
-    end
   end
 end
