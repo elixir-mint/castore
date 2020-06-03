6ece9401e6262e26da15e17dce0db97dfd4615f8
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2020-06-03 16:03:28 +0200

    Filter expired certificates automatically (#19)
    Co-authored-by: Bram Verburg <bram.verburg@voltone.net>

--- lib/mix/tasks/certdata.ex
+++ lib/mix/tasks/certdata.ex
@@ -20,31 +20,54 @@ defmodule Mix.Tasks.Certdata do
 
   use Mix.Task
 
+  require Record
+
+  Record.defrecord(
+    :certificate,
+    :Certificate,
+    Record.extract(:Certificate, from_lib: "public_key/include/OTP-PUB-KEY.hrl")
+  )
+
+  Record.defrecord(
+    :tbs_certificate,
+    :TBSCertificate,
+    Record.extract(:TBSCertificate, from_lib: "public_key/include/OTP-PUB-KEY.hrl")
+  )
+
+  Record.defrecord(
+    :validity,
+    :Validity,
+    Record.extract(:Validity, from_lib: "public_key/include/OTP-PUB-KEY.hrl")
+  )
+
   @shortdoc "Fetches an up-to-date version of the CA certificate store"
 
   @mk_ca_bundle_url "https://raw.githubusercontent.com/curl/curl/master/lib/mk-ca-bundle.pl"
   @mk_ca_bundle_cmd "mk-ca-bundle.pl"
   @ca_bundle "ca-bundle.crt"
   @ca_bundle_target "priv/cacerts.pem"
-  @cert_exceptions ["AddTrust External Root"]
 
   @impl true
   def run(args)
 
   def run([]) do
-    fetch_ca_bundle()
-    apply_exceptions()
-    File.rename(@ca_bundle, @ca_bundle_target)
+    bundle =
+      fetch_ca_bundle()
+      |> parse_bundle()
+      |> filter_expired()
+      |> rebuild_bundle()
+
+    File.write!(@ca_bundle_target, bundle)
   end
 
   def run(["--check-outdated"]) do
-    fetch_ca_bundle()
-    apply_exceptions()
+    new_bundle =
+      fetch_ca_bundle()
+      |> parse_bundle()
+      |> filter_expired()
 
-    old_bundle = read_certificates_set(CAStore.file_path())
-    new_bundle = read_certificates_set(@ca_bundle)
-
-    File.rm!(@ca_bundle)
+    old_bundle = read_certificates_set(parse_bundle(File.read!(CAStore.file_path())))
+    new_bundle = read_certificates_set(new_bundle)
 
     if not MapSet.equal?(old_bundle, new_bundle) do
       Mix.raise("#{CAStore.file_path()} is outdated. Run \"mix certdata\" to update it.")
@@ -61,20 +84,11 @@ defmodule Mix.Tasks.Certdata do
     try do
       File.chmod!(@mk_ca_bundle_cmd, 0o755)
       cmd!("#{Path.expand(@mk_ca_bundle_cmd)} -u")
+      File.read!(@ca_bundle)
     after
       File.rm!(@mk_ca_bundle_cmd)
+      File.rm!(@ca_bundle)
     end
-  end
-
-  defp apply_exceptions() do
-    bundle = File.read!(@ca_bundle)
-
-    bundle =
-      Enum.reduce(@cert_exceptions, bundle, fn cert, bundle ->
-        Regex.replace(~r"\n#{cert}.*-----END CERTIFICATE-----\n"Us, bundle, "")
-      end)
-
-    File.write!(@ca_bundle, bundle)
   end
 
   defp cmd!(cmd) do
@@ -87,10 +101,77 @@ defmodule Mix.Tasks.Certdata do
     end
   end
 
-  defp read_certificates_set(path) do
-    path
-    |> File.read!()
-    |> :public_key.pem_decode()
-    |> MapSet.new()
+  defp read_certificates_set({_comments, bundle}) do
+    MapSet.new(bundle)
+  end
+
+  defp parse_bundle(bundle) do
+    lines = String.split(bundle, "\n", trim: true)
+    {comments, bundle} = Enum.split_while(lines, &String.starts_with?(&1, "#"))
+    {comments, chunk_certs(bundle, [])}
+  end
+
+  defp chunk_certs([name, "=" <> _ | lines], certs) do
+    {cert, [_ | lines]} = Enum.split_while(lines, &(&1 != "-----END CERTIFICATE-----"))
+    cert = {name, Enum.join(cert ++ ["-----END CERTIFICATE-----"], "\n")}
+    chunk_certs(lines, [cert | certs])
+  end
+
+  defp chunk_certs([], certs) do
+    Enum.reverse(certs)
+  end
+
+  defp rebuild_bundle({comments, certs}) do
+    certs =
+      Enum.map(certs, fn {name, cert} ->
+        [name, ?\n, :lists.duplicate(String.length(name), ?=), ?\n, cert]
+      end)
+
+    [
+      Enum.intersperse(comments, ?\n),
+      "\n\n\n",
+      Enum.intersperse(certs, "\n\n"),
+      ?\n
+    ]
+  end
+
+  defp filter_expired({comments, certs}) do
+    certs = Enum.reject(certs, fn {_name, cert} -> expired?(:public_key.pem_decode(cert)) end)
+    {comments, certs}
+  end
+
+  defp expired?([pem_entry]) do
+    not_after =
+      pem_entry
+      |> :public_key.pem_entry_decode()
+      |> certificate(:tbsCertificate)
+      |> tbs_certificate(:validity)
+      |> validity(:notAfter)
+      |> parse_asn1_date_time()
+
+    DateTime.to_unix(not_after) < DateTime.to_unix(DateTime.utc_now())
+  end
+
+  # Technically we would have to acount for utcTime values between 1950 and
+  # 2000, but I think it is safe to ignore that here
+  defp parse_asn1_date_time({:utcTime, date_time}) when length(date_time) == 13 do
+    parse_asn1_date_time({:generalTime, [?2, ?0 | date_time]})
+  end
+
+  defp parse_asn1_date_time({:generalTime, date_time}) when length(date_time) == 15 do
+    [y1, y2, y3, y4, m1, m2, d1, d2 | _] = date_time
+
+    %DateTime{
+      year: List.to_integer([y1, y2, y3, y4]),
+      month: List.to_integer([m1, m2]),
+      day: List.to_integer([d1, d2]),
+      hour: 0,
+      minute: 0,
+      second: 0,
+      time_zone: "Etc/UTC",
+      zone_abbr: "UTC",
+      utc_offset: 0,
+      std_offset: 0
+    }
   end
 end
