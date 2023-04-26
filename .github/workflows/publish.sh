bff0d32410a68ad295df5faa55b2cc06bd83acbc
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2023-04-27 00:08:08 +0200

    Format

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -8,7 +8,7 @@ if [[ "${changed_version}" ]]; then
   git config user.name "${GITHUB_ACTOR}"
   git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
   git tag "v$(shell cat VERSION)"
-	git push --tags
+  git push --tags
 
   mix hex.publish --yes
 else
