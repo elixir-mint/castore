4fae1c531ded22406edbc8918cbfacc347dbe6c6
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2023-04-27 00:07:25 +0200

    Push tag

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -4,6 +4,12 @@ changed_version="$(git diff --name-only HEAD~1 HEAD -- VERSION)"
 
 if [[ "${changed_version}" ]]; then
   echo "Publishing because the version changed since the last commit"
+
+  git config user.name "${GITHUB_ACTOR}"
+  git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+  git tag "v$(shell cat VERSION)"
+	git push --tags
+
   mix hex.publish --yes
 else
   echo "Not publishing since the version didn't change since last commit"
