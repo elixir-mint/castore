a4011db3fbcf2649c3a6141a833c25f3dda8a62c
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2023-04-27 00:00:59 +0200

    Format

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -3,8 +3,8 @@
 changed_version="$(git diff --name-only HEAD~1 HEAD -- VERSION)"
 
 if [[ "${changed_version}" ]]; then
-	echo "Publishing because the version changed since the last commit"
+  echo "Publishing because the version changed since the last commit"
   mix hex.publish --yes
 else
-	echo "Not publishing since the version didn't change since last commit"
+  echo "Not publishing since the version didn't change since last commit"
 fi
