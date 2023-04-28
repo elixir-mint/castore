c28a433bf07fcee7bbc96a4ed1c0b3ae0054f1b1
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2023-04-28 13:44:04 +0200

    Bump VERSION in certificate update PR

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 changed_version="$(git diff --name-only HEAD~1 HEAD -- VERSION)"
 
 if [[ "${changed_version}" ]]; then
   echo "Publishing because the version changed since the last commit"
 
-  git config user.name "${GITHUB_ACTOR}"
-  git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
   git tag "v$(cat VERSION)"
   git push --tags
 
