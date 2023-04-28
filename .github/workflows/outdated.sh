9a7f4bcd091eaf357ca305030fc4a029b62cec01
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2023-04-28 16:40:28 +0100

    Bump VERSION in certificate update PR (#57)

--- .github/workflows/outdated.sh
+++ .github/workflows/outdated.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-git config user.name "GitHub Actions"
-git config user.email "actions@users.noreply.github.com"
+function bump_version {
+  awk -F. '{printf("%d.%d.%d\n", $1, $2, $3+1) >"VERSION"}' VERSION
+}
+
+function check_pr {
+  gh pr list --state open --label "outdated check"
+}
+
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git checkout outdated || git checkout -b outdated
 
 if ! mix certdata --check-outdated; then
   mix certdata
+
+  if [[ $(check_pr) == "" ]]; then
+    bump_version
+  fi
+
   git add .
   git commit -m "Update certificates"
   git push --set-upstream origin outdated
 
-  if [[ $(gh pr list --state open --label "outdated check") == "" ]]; then
+  if [[ $(check_pr) == "" ]]; then
     gh pr create --fill --label "outdated check"
   fi
 fi
